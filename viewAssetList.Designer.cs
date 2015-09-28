40a9f88030d204103263675f7125140d51671d8c
OiCMudkips <victor_zhou1995@hotmail.ca>
2015-09-28 02:38:19 -0400

    add missing files for view asset list

---
+++ viewAssetList.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Marimba
+{
+    partial class viewAssetList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.assetListView = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.currentValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.originalValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // assetListView
+            // 
+            this.assetListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.currentValue,
+            this.originalValue});
+            this.assetListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.assetListView.FullRowSelect = true;
+            this.assetListView.Location = new System.Drawing.Point(0, 0);
+            this.assetListView.MultiSelect = false;
+            this.assetListView.Name = "assetListView";
+            this.assetListView.Size = new System.Drawing.Size(664, 441);
+            this.assetListView.TabIndex = 0;
+            this.assetListView.UseCompatibleStateImageBehavior = false;
+            this.assetListView.View = System.Windows.Forms.View.Details;
+            this.assetListView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.viewAssetList_ColumnClick);
+            // 
+            // name
+            // 
+            this.name.Text = "Name";
+            this.name.Width = 385;
+            // 
+            // currentValue
+            // 
+            this.currentValue.Text = "Current Value";
+            this.currentValue.Width = 125;
+            // 
+            // originalValue
+            // 
+            this.originalValue.Text = "Original Value";
+            this.originalValue.Width = 125;
+            // 
+            // viewAssetList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 441);
+            this.Controls.Add(this.assetListView);
+            this.Font = new System.Drawing.Font("Quicksand", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "viewAssetList";
+            this.Text = "Asset List";
+            this.Load += new System.EventHandler(this.viewAssetList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView assetListView;
+        private System.Windows.Forms.ColumnHeader currentValue;
+        private System.Windows.Forms.ColumnHeader originalValue;
+        private System.Windows.Forms.ColumnHeader name;
+    }
+}
---
+++ viewAssetList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marimba
+{
+    public partial class viewAssetList : Form
+    {
+
+        public ListViewColumnSorter lvmColumnSorter;
+
+        public viewAssetList()
+        {
+            InitializeComponent();
+        }
+
+        private void viewAssetList_Load(object sender, EventArgs e)
+        {
+            assetListView.BeginUpdate();
+
+            // reset sorting
+            lvmColumnSorter = new ListViewColumnSorter();
+            this.assetListView.ListViewItemSorter = lvmColumnSorter;
+
+            // get the indices of all assets in the budget
+            int[] assetIndexes = clsStorage.currentClub.assetList(true);
+
+            List<ListViewItem> assetList = new List<ListViewItem>();
+            ListViewItem item;
+            for (int i = 0; i < assetIndexes.Length; i++)
+            {
+                // get the current budget item
+                budgetItem currentBudgetItem = clsStorage.currentClub.budget[assetIndexes[i]];
+
+                // get the fields that will eb displayed
+                string[] itemText = new string[3];
+                itemText[0] = currentBudgetItem.name;
+                itemText[1] = clsStorage.currentClub.valueAfterDepreciation(assetIndexes[i]).ToString("C");
+                itemText[2] = currentBudgetItem.value.ToString("C");
+
+                // add the current item to the list to be displayed
+                item = new ListViewItem(itemText);
+                assetList.Add(item);
+            }
+
+            // update the list
+            assetListView.Items.AddRange(assetList.ToArray());
+            assetListView.EndUpdate();
+        }
+
+        private void viewAssetList_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == lvmColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvmColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvmColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvmColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvmColumnSorter.SortColumn = e.Column;
+                lvmColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.assetListView.Sort();
+            if (Properties.Settings.Default.playSounds)
+                sound.click.Play();
+        }
+    }
+}
