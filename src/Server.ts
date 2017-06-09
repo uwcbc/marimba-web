23f8bd82665ae7b620042f684d72c882ecd78dc7
Victor Zhou <OiCMudkips@users.noreply.github.com>
2017-06-09 18:17:08 -0400

    made stuff work with typescript, CAS

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,15 @@
 import express = require('express');
 import mysql = require('mysql');
+import CAS = require('cas-authentication');
 
 class Server {
 	private express : express.Application;
+	private cas;
 
 	constructor() {
 		this.express = express();
 		this.configureRoutes();
+		this.configureCAS();
 	}
 
 	private configureRoutes() : void {
@@ -15,6 +18,14 @@ class Server {
 			resp.json({ message: 'Hello world!' });
 		});
 		this.express.use('/', router);
+	}
+
+	private configureCAS() : void {
+		this.cas = new CAS({
+			cas_url: 'https://cas-dev.uwaterloo.ca/cas',
+			service_url: 'https://redir-loo.com',
+			cas_version: '2.0'
+		});
 	}
 
 	public start() : void {
