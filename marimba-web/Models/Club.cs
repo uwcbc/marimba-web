7953a8d0164d8303a64fd178244b61ead57f7733
NATHAN LIU <dingming1996@gmail.com>
2020-01-22 20:25:56 -0500

    Added Club class to handle adding terms/members to the database.

---
+++ marimba-web/Models/Club.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace marimba_web.Models
+{
+    public static class Club
+    {
+
+    }
+}
