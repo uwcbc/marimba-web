ffff3d59b2a3e95db59f8a40a00d454cf53ff638
JeffreyCA <jeffreyca16@gmail.com>
2020-01-25 21:46:41 -0500

    Add Elector class

---
+++ marimba-web/Models/Elector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace marimba_web.Models
+{
+    public class Elector
+    {
+        public string name { get; set; }
+        public MailAddress email { get; set; }
+        public bool isPaidMembership { get; set; }
+    }
+}
