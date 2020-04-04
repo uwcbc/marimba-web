2196896a2d0bc41b26520b0af5dd964a69eca0f9
JeffreyCA <jeffreyca16@gmail.com>
2020-04-03 20:25:59 -0400

    Implement attendance retrieval, editing and exporting

--- marimba-web/Controllers/MembersController.cs
+++ marimba-web/Controllers/MembersController.cs
@@ -55,6 +55,63 @@ namespace marimba_web.Controllers
             return await context.Members.ToListAsync();
         }
 
+        /// <summary>
+        /// Get the attendance records for the given Term.
+        /// </summary>
+        /// <returns>
+        /// TermAttendance record for the given Term. It contains a list of all rehearsals as well as
+        /// the main attendance table.
+        /// </returns>
+        [HttpGet]
+        public IActionResult GetAttendanceForTerm(string termName)
+        {
+            Term term = context.Terms.First(t => t.name == termName);
+            return new ObjectResult(new TermAttendance(term));
+        }
+
+        /// <summary>
+        /// Get the attendance record for the given Term as a CSV file.
+        /// </summary>
+        /// <param name="term">The Term</param>
+        /// <returns>CSV file</returns>
+        public IActionResult GetAttendanceAsCsv(Term term)
+        {
+            byte[] csvBytes = new TermAttendance(term).WriteCsvBytes();
+            string fileName = term.name + ".csv";
+            return File(csvBytes, "text/csv", fileName);
+        }
+
+        /// <summary>
+        /// Update a Term's attendance record for a particular Member for a particular Rehearsal.
+        /// </summary>
+        /// <param name="termName">Term name</param>
+        /// <param name="memberId">Member GUID</param>
+        /// <param name="rehearsalDate">Rehearsal date</param>
+        /// <param name="isPresent">Whether the Member should be marked as present</param>
+        /// <returns>OkResult if successful</returns>
+        [HttpPost]
+        public IActionResult UpdateAttendanceForTerm(string termName, Guid memberId, DateTime rehearsalDate, bool isPresent)
+        {
+            Term term = context.Terms.First(t => t.name == termName);
+            // TODO: Consider defining a universal date-time format
+            Rehearsal rehearsal = term.rehearsals.First(r => r.date == rehearsalDate);
+            bool containsMember = rehearsal.members.Contains(memberId);
+
+            // Update attendance
+            if (containsMember && !isPresent)
+            {
+                rehearsal.members.Remove(memberId);
+            }
+            else if (!containsMember && isPresent)
+            {
+                rehearsal.members.Add(memberId);
+            }
+
+            context.SaveChanges();
+            return new OkResult();
+        }
+
+
         public MembersController(MarimbaDbContext context)
         {
             this.context = context;
