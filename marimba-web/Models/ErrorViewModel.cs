9de3991a2d94488149b401caf1ba5732e1a7fc45
NATHAN LIU <dingming1996@gmail.com>
2021-12-14 23:30:15 -0800

    Initial commit: clean workspace.

--- marimba-web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace marimba_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
